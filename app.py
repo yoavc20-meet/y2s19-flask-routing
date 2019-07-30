07edd86a6517aa7fb3d880702d300ea7ade4895f
myname21-meet <myname21@meet.mit.edu>
2019-07-30 10:30:44 +0300

    flask

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "home page" 
+	return render_template('home.html')
+
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+	return render_template('student.html', id_number = student_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
